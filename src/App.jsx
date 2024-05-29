15289c5fe70a18ac9f43a7137a2771ae2d62c0f6
Andreia Sá <andreiapsa6@gmail.com>
2024-05-29 17:51:10 +0200

    json file, usestate and .map added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import ToDoListJson from "../src/assets/to-do-list.json"
+import { useState } from 'react'
 
 function App() {
-
-  return (
+const [list, setList] = useState(ToDoListJson)
+  return (    
     <>
-      <Navbar />
-    <Sidebar />
-    <Footer />
+  <Navbar />
+
+    <div>
+      {list.map((oneList)=>{
+        return (
+          <div key={oneList.id} className='list-card'>
+          <h3>{oneList.task}</h3>
+          <p>{oneList.completed}</p>
+          </div>
+        )
+      
+})}
+ </div>
+    
+      
+      <Sidebar />
+      <Footer />
     </>
   )
 }
